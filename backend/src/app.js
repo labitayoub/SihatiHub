eace06eb704cdcddc8242cf23a5f4455a61d44a7
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2025-10-10 09:42:00 +0100

    feat(app): wire middlewares and routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,17 @@
 import express from 'express';
-import connectDB from './config/db.js';
-import userRoute from './routes/userRoute.js'
-
+import userRoute from './routes/userRoute.js';
+import appointmentRoute from './routes/appointmentRoute.js';
 
 const app = express();
-const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB();
-
 app.use('/user', userRoute);
+app.use('/rendez-vous', appointmentRoute);
 
-app.get("/",(req,res)=>{
-    res.send("<h2>Hello Ayoub</h2>")
-})
-
-app.listen(port, () => console.log(`listening on port http://localhost:${port}`))
+app.get('/', (req, res) => {
+    res.send('<h2>SihatiHub API</h2>');
+});
 
 export default app;
